bcaea81f2acb19a52c01ef0d81d476e0bfa6878d
Adam Cox <adam@adamcox.net>
2020-07-08 21:16:13 -0400

    add db context service and connection string

--- Wingtiptoys/Startup.cs
+++ Wingtiptoys/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,8 @@ namespace Wingtiptoys
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddDbContext<Models.wingtiptoysContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("wingtiptoys")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
